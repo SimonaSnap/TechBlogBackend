6f3b8e28c74a80e8a869a2a59536e8184d7e106a
Simona <simona.snapk@gmail.com>
2022-06-01 22:52:18 -0700

    posts done, need to figure out comments

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,7 +1,47 @@
 const router = require('express').Router();
-const { User, Post, Comments } = require('../../models');
+const { Post } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+router.post("/", withAuth, async (req, res) =>
+{
+    try
+    {
+        const newPost = await Post.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
 
+        res.status(200).json(newPost)
+    } catch (err)
+    {
+        console.log(err);
+        res.status(400).json(err);
+    }
+});
+
+router.delete("/:id", withAuth, async (req, res) =>
+{
+    try
+    {
+        const postData = await Post.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        });
+
+        if (!postData)
+        {
+            res.status(404).json({ message: "No Post at this id" });
+            return;
+        }
+
+        res.status(200).json(postData);
+    } catch (err)
+    {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
